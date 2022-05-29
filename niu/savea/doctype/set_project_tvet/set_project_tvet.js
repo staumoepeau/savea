8ab3bd775af2bcefda028d8ec0080bb3395599d6
staumoepeau <sione.taumoepeau@gmail.com>
2022-05-29 22:06:59 +1300

    Create Repo for Savea

---
+++ niu/savea/doctype/set_project_tvet/set_project_tvet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Finau Hoi Taumoepeau and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SET Project TVET', {
+	// refresh: function(frm) {
+
+	// }
+});
